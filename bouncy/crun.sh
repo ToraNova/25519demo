4d94fb01a76f55dc3dd51d68a871ed492819b20f
ToraNova <chia_jason96@live.com>
2019-11-14 03:11:57 +0800

    added extjars for standalone-ness

--- bouncy/crun.sh
+++ bouncy/crun.sh
@@ -4,11 +4,12 @@
 # quick script to compile and run for testing
 
 if [ -z "$1" ]; then
-	javac -cp "/usr/share/java/bcprov-ext-jdk13-164.jar:/usr/share/java/bcpkix-jdk13-164.jar" BC25519.java
+	#javac -cp "/usr/share/java/bcprov-ext-jdk13-164.jar:/usr/share/java/bcpkix-jdk13-164.jar" BC25519.java
+	javac -cp "../extjars/bcprov-ext.jar:../extjars/bcpkix.jar" BC25519.java
 	echo Compiled BC25519.java
 	exit 0
 fi
 
-java -cp ".:/usr/share/java/bcprov-ext-jdk13-164.jar:/usr/share/java/bcpkix-jdk13-164.jar" BC25519 $1
+java -cp ".:../extjars/bcprov-ext.jar:../extjars/bcpkix.jar" BC25519 $1
 exit 0
 
